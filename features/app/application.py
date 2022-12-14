c7169b1a351f8cc2fb9f6f5cd73e808a0a2c4de8
Hammad Chaudhry <hammadchaudhry@Hammads-MacBook-Pro.local>
2022-12-14 15:18:52 -0500

    Adding framework

---
+++ features/app/application.py
@@ -0,0 +1,12 @@
+
+
+
+
+class Application:
+
+    def __init__(self, driver):
+        self.driver = driver
+        # self.bestsellers_page = BestsellersPage(self.driver)
+        # self.main_page = MainPage(self.driver)
+        # self.search_results_page = SearchResultsPage(self.driver)
+        # self.sign_in_page = SignInPage(self.driver)
